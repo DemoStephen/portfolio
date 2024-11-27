b8a82580fc57fbbb5309e548350441c4c4994d56
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-28 00:55:06 +0100

    Used React Router for the very first time

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
+import About from "./Pages/About";
 import Home from "./Pages/Home";
-import Navigation from "./UI/Navigation";
-
-function App() {
-  return (
-    <section className="flex relative text-slate-50 justify-start  flex-col-reverse md:flex-col lg:flex-row h-screen">
-      <Navigation />
-      <Home />
-    </section>
-  );
+import Projects from "./Pages/Projects";
+import Error from "./Pages/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Services from "./Pages/Services";
+import TechStacks from "./Pages/TechStacks";
+import BlogPost from "./Pages/BlogPost";
+import Articles from "./Pages/Article";
+import ContactPage from "./Pages/Contacts";
+const rounter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Projects",
+    element: <Projects />,
+  },
+  {
+    path: "/Services",
+    element: <Services />,
+  },
+  {
+    path: "/Stack",
+    element: <TechStacks />,
+  },
+  {
+    path: "/Blog",
+    element: <BlogPost />,
+  },
+  {
+    path: "/Articles",
+    element: <Articles />,
+  },
+  {
+    path: "/Contact",
+    element: <ContactPage />,
+  },
+]);
+export default function App() {
+  return <RouterProvider router={rounter} />;
 }
-
-export default App;
