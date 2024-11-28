aea63adea0d658f960c87ed1adf4da598372316a
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-28 01:26:56 +0100

    Fixed bug on the list Item

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -36,31 +36,35 @@ export default function List({
           </li>
         </a>
       ) : (
-        <Link to={`/${tagline}`}>
-          <li
-            className={`border w-full lg:w-auto p-2 ${
-              !view ? "flex" : "lg:hidden"
-            } justify-center border-dashed ${
-              sideBarWidth ? "" : "lg:justify-between"
-            } items-center content-center hover:text-slate-50 border hover:border-slate-700 hover:bg-gray-800 transition-all cursor-pointer`}
-          >
-            {sideBarWidth ? (
-              <p className="flex items-center content-center gap-0.5 lg:gap-2 flex-col lg:flex-row">
-                <span>{icon}</span>
-              </p>
-            ) : (
-              <>
+        <li
+          className={`flex justify-center lg:block w-full border lg:w-auto hover:border-slate-700 hover:bg-gray-800 transition-all cursor-pointer hover:text-slate-50 ${
+            !view ? "flex" : "lg:hidden"
+          }`}
+        >
+          <Link to={`/${tagline}`}>
+            <button
+              className={`w-full items-center content-center p-2 justify-center border-dashed ${
+                !view ? "flex" : "lg:hidden"
+              } ${sideBarWidth ? "" : "lg:justify-between"}`}
+            >
+              {sideBarWidth ? (
                 <p className="flex items-center content-center gap-0.5 lg:gap-2 flex-col lg:flex-row">
                   <span>{icon}</span>
-                  <span className="text-xs lg:text-base">{tagline}</span>
                 </p>
-                <span className="hidden w-6 justify-center lg:flex border border-slate-500 rounded-md">
-                  {index}
-                </span>
-              </>
-            )}
-          </li>
-        </Link>
+              ) : (
+                <>
+                  <p className="flex items-center content-center gap-0.5 lg:gap-2 flex-col lg:flex-row">
+                    <span>{icon}</span>
+                    <span className="text-xs lg:text-base">{tagline}</span>
+                  </p>
+                  <span className="hidden w-6 justify-center lg:flex border border-slate-500 rounded-md">
+                    {index}
+                  </span>
+                </>
+              )}
+            </button>
+          </Link>
+        </li>
       )}
     </>
   );
