c1d20c4aa4580fd4292c280a773c5774579cc974
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-11-28 10:03:18 +0100

    Fixed React Rounter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,59 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import Error from "./Pages/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Services from "./Pages/Services";
+import TechStacks from "./Pages/TechStacks";
+import BlogPost from "./Pages/BlogPost";
+import Articles from "./Pages/Article";
+import ContactPage from "./Pages/Contacts";
+const rounter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Projects",
+    element: <Projects />,
+  },
+  {
+    path: "/Services",
+    element: <Services />,
+  },
+  {
+    path: "/Stack",
+    element: <TechStacks />,
+  },
+  {
+    path: "/Blog",
+    element: <BlogPost />,
+  },
+  {
+    path: "/Articles",
+    element: <Articles />,
+  },
+  {
+    path: "/Contact",
+    element: <ContactPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={rounter} />
   </StrictMode>
 );
