7742e51bcf4e2eebe09ea4cb9d0f8d2a475aa370
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-01 17:56:47 +0100

    Tech Stack Updated

---
+++ src/UI/TechStack.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import Stack from "../Components/Stack";
+export default function TechStack({ title, stack }) {
+  return (
+    <section className="border p-4 border-dashed rounded-lg">
+      <h2 className="text-2xl lg:text-3xl mb-4">{title}</h2>
+      <section>
+        <div className="grid md:grid-cols-2 gap-5">
+          {stack.map((info) => {
+            return (
+              <Stack
+                key={info.name}
+                image={info.image}
+                name={info.name}
+                about={info.about}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </section>
+  );
+}
