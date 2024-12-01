cbd02c7e416edb0a15017c3869aaf2c1f59a837b
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-01 17:59:28 +0100

    Updated website description

---
+++ src/util.js
@@ -0,0 +1,59 @@
+import figma from "./assets/Image/stacks/figma.png";
+import photoshop from "./assets/Image/stacks/photoshop.png";
+export const STACKS = [
+  {
+    title: "Design",
+    stack: [
+      {
+        image: figma,
+        name: "Figma",
+        about: "Design tool",
+      },
+      {
+        image: photoshop,
+        name: "Photoshop",
+        about: "Image Editing Software",
+      },
+    ],
+  },
+  {
+    title: "Development",
+    stack: [
+      {
+        image: figma,
+        name: "VS Code",
+        about: "Code Editor",
+      },
+      {
+        image: figma,
+        name: "Git",
+        about: "Version Control system",
+      },
+    ],
+  },
+  {
+    title: "Productivity",
+    stack: [
+      {
+        image: figma,
+        name: "Slack",
+        about: "Collaboration",
+      },
+    ],
+  },
+  {
+    title: "Artificial Intelligence",
+    stack: [
+      {
+        image: figma,
+        name: "ChatGPT",
+        about: "OpenAI",
+      },
+      {
+        image: figma,
+        name: "Poe",
+        about: "Virtual Assistant",
+      },
+    ],
+  },
+];
