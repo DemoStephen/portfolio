a7dcb83b9b0398b7ed4a990f787d28198a30112b
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-07 21:47:50 +0100

    Updated Services file

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
-import img from "../assets/Image/stacks/js.png";
-export default function Service({ heading, text }) {
+export default function Service({ heading, text, img }) {
   return (
     <section className="border border-dashed p-4 rounded-lg">
-      <img src={img} alt="" className="w-16" />
+      <img src={img} alt="illustration" className="w-16" />
       <header className="my-5">
         <h3 className="text-3xl mb-4">{heading}</h3>
         <p>{text}</p>
--- src/UI/ServicesPreview.jsx
+++ src/UI/ServicesPreview.jsx
@@ -1,23 +1,25 @@
 import Service from "../Components/Service";
-import img from "../assets/Image/stacks/js.png";
-
+import damn from "../assets/Image/stacks/react.png";
 export default function ServicesPreview() {
   return (
     <section className="px-[5%] md:px-[10%] grid lg:grid-cols-2 gap-8">
       <Service
+        img={damn}
         heading="Web Design and Development"
         text="Whether it is adding a new feature to a current website, or a custom website from scratch, then I can help. All my work is tailored to suit the needs of the project. I work in an agile fashion, building websites to cater for the content and the audience they appeal to."
       />
       <Service
+        img={damn}
         heading="Website performance audits"
         text="I possess extensive knowledge in web design and development and am now offering website performance audits as a service. Through a comprehensive set of tests, I will identify performance improvements to help minimize your website's impact."
       />
       <Service
+        img={damn}
         heading="Consultation"
         text="Are you in need of advice regarding green website hosting? Need a second opinion about a website build? Whether you need help with writing a brief, scoping a job, or a full low-carbon website redesign strategy, I can assist with my consultation service."
       />
       <section className="border border-dashed p-4 rounded-lg">
-        <img src={img} alt="" className="w-16" />
+        <img src={damn} alt="illustration" className="w-16" />
         <header className="my-5">
           <p>See the proof</p>
           <h3 className="text-3xl mb-4">View my work</h3>
