a47ada706a94c3492df07e597ac32119177a5528
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-08 22:38:38 +0100

    background added to navigation

--- src/UI/Navigation.jsx
+++ src/UI/Navigation.jsx
@@ -26,9 +26,9 @@ export default function Navigation() {
   return (
     <nav
       style={{ scrollbarWidth: "none" }}
-      className={`flex flex-col justify-between border-dashed font-medium w-full ${
+      className={`bg-slate-900 flex flex-col justify-between border-dashed font-medium w-full ${
         sideBarWidth ? "lg:w-20" : "lg:w-64 xl:w-72 2xl:w-1/5"
-      } px-[5%] py-1 lg:px-2 lg:py-4 h-auto lg:min-h-screen sticky md:top-0 bottom-0 lg:relative border lg:overflow-scroll gap-4 z-50 transition-all`}
+      } px-[5%] py-1 lg:px-2 lg:py-4 h-auto lg:min-h-screen sticky md:top-0 bottom-0 lg:relative md:border lg:overflow-scroll gap-4 z-50 transition-all`}
     >
       <ColapseNavBar
         handleSideBarWidth={handleSideBarWidth}
