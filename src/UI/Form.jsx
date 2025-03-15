2afa8a619487300594493b8506f0fdc312126f9c
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-03-15 01:46:29 +0100

    formspree library implemented for processing form submission

--- src/UI/Form.jsx
+++ src/UI/Form.jsx
@@ -1,7 +1,18 @@
+import { useForm } from "@formspree/react";
 export default function Form() {
+  const [state, handleSubmit] = useForm("xeoaoojp");
+
+  if (state.succeeded) {
+    return (
+      <div className="border border-dashed lg:w-4/5 m-auto p-4 rounded-lg text-center">
+        <p>You message is sent successfully. We will get back to you soon.</p>
+      </div>
+    );
+  }
+
   return (
     <form
-      action="#"
+      onSubmit={handleSubmit}
       className="border border-dashed lg:w-4/5 m-auto p-4 rounded-lg flex flex-col gap-4"
     >
       <h2 className="text-2xl">Send a message</h2>
@@ -13,6 +24,7 @@ export default function Form() {
             id="name"
             placeholder="Name"
             className="w-full p-4 bg-transparent border rounded-lg outline-none"
+            required
           />
         </label>
         <label htmlFor="email" className="w-full">
@@ -22,6 +34,7 @@ export default function Form() {
             id="email"
             placeholder="Email"
             className="w-full p-4 bg-transparent border rounded-lg outline-none"
+            required
           />
         </label>
       </div>
@@ -30,8 +43,13 @@ export default function Form() {
         id="message"
         placeholder="Message"
         className="min-h-40 rounded-lg bg-transparent border p-4"
+        required
       ></textarea>
-      <button type="submit" className="border p-4 rounded-lg hover:bg-slate-800">
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className="border p-4 rounded-lg hover:bg-slate-800"
+      >
         Send Message
       </button>
     </form>
