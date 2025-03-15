1c953e5019f517811891ec2a547480312517812a
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-03-15 01:57:22 +0100

    updated navigation for mobile screen

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./UI/Navigation";
-
+import me from "./assets/me.jpg";
 export default function Layout() {
   return (
     <>
@@ -10,6 +10,17 @@ export default function Layout() {
           style={{ scrollbarWidth: "none" }}
           className="lg:pt-16 w-full md:border border-dashed max-h-screen overflow-scroll snap-none"
         >
+          <nav className="flex lg:hidden sticky top-0 bg-slate-900 items-center gap-4 py-4 border-y border-dashed px-[5%] z-50">
+            <img
+              src={me}
+              alt=""
+              className="w-10 h-10 rounded-full bg-slate-50"
+            />
+            <div>
+              <h3 className="-mb-1">Stephen Adewale</h3>
+              <p className="text-sm">FrontEnd Developer</p>
+            </div>
+          </nav>
           <Outlet />
         </main>
       </section>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,9 @@
 import Footer from "../UI/Footer";
 import Header from "../UI/Header";
 import { Stacks, Thoughts } from "../UI/HomePreview";
-import me from "../assets/me.jpg";
 export default function Home() {
   return (
     <>
-      <nav className="flex lg:hidden items-center gap-4 py-4 border-y border-dashed px-[5%]">
-        <img src={me} alt="" className="w-10 h-10 rounded-full bg-slate-50" />
-        <div>
-          <h3 className="-mb-1">Stephen Adewale</h3>
-          <p className="text-sm">FrontEnd Developer</p>
-        </div>
-      </nav>
       <Header />
       <Thoughts />
       <Stacks />
